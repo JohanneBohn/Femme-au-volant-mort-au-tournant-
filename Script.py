4687667250add2e309badd6cb2cc7d8721c12558
joh <johanne.bohn@ensae.fr>
2025-12-29 22:30:26 +0000

    script cleaner

--- Script.py
+++ Script.py
@@ -56,15 +56,15 @@ def plot_roc_auc(
     if y_prob_rf is not None:
         fpr_rf, tpr_rf, _ = roc_curve(y_test, y_prob_rf)
         roc_auc = auc(fpr_rf, tpr_rf)
-        plt.plot(fpr_rf, tpr_rf, color='green', label=f"AUC = {roc_auc:.2f}")
+        plt.plot(fpr_rf, tpr_rf, color='green', label=f"AUC - Random forest = {roc_auc:.2f}")
     if y_prob_xgb is not None:
         fpr_xgb, tpr_xgb, _ = roc_curve(y_test, y_prob_xgb)
         roc_auc = auc(fpr_xgb, tpr_xgb)
-        plt.plot(fpr_xgb, tpr_xgb, color='blue', label=f"AUC = {roc_auc:.2f}")
+        plt.plot(fpr_xgb, tpr_xgb, color='blue', label=f"AUC - XG Boost = {roc_auc:.2f}")
     if y_prob_nn is not None:
         fpr_nn, tpr_nn, _ = roc_curve(y_test, y_prob_nn)
         roc_auc = auc(fpr_nn, tpr_nn)
-        plt.plot(fpr_nn, tpr_nn, color='purple', label=f"AUC = {roc_auc:.2f}")
+        plt.plot(fpr_nn, tpr_nn, color='purple', label=f"AUC - Réseau de neurones = {roc_auc:.2f}")
     plt.plot([0, 1], [0, 1], linestyle="--", color="grey")
     plt.xlabel("Taux de faux positifs")
     plt.ylabel("Taux de vrais positifs")
@@ -72,3 +72,67 @@ def plot_roc_auc(
     plt.legend()
     plt.show()
     return None
+
+def xg_boost(
+    X_train,
+    X_test,
+    y_train,
+    y_test
+):
+    model = xgb.XGBClassifier(
+        n_estimators=200,
+        max_depth=5,
+        learning_rate=0.1,
+        scale_pos_weight= (len(y_train)-sum(y_train)) / sum(y_train),
+        random_state=42,
+        eval_metric='logloss'
+    )
+    model.fit(X_train, y_train)
+    y_prob = model.predict_proba(X_test)[:,1]
+    y_pred = (y_prob >= 0.5).astype(int)
+    print(classification_report(y_test, y_pred))
+    return y_pred, y_prob
+
+def rn(
+    X_train,
+    X_test,
+    y_train,
+    y_test
+):
+    # Standardisation des variables continues ('vma' et 'age', aux colonnes 3 et 7) :
+    scaler = StandardScaler()
+    X_train[:, [3,7]] = scaler.fit_transform(X_train[:, [3,7]])
+    X_test[:, [3,7]] = scaler.transform(X_test[:, [3,7]])
+    # Ajustement des poids :
+    class_weights_array = compute_class_weight(
+        class_weight='balanced',
+        classes=np.unique(y_train),
+        y=y_train
+    )
+    class_weights = dict(enumerate(class_weights_array))
+    # Création du modèle
+    model = Sequential([
+        Input(shape=(X_train.shape[1],)),
+        Dense(32, activation='relu'),
+        Dropout(0.3),
+        Dense(16, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+    model.compile(
+        optimizer=Adam(learning_rate=0.001),
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+    # Entraînement :
+    history = model.fit(
+        X_train, y_train,
+        validation_split=0.2,
+        epochs=50,
+        batch_size=32,
+        class_weight=class_weights,
+        verbose=1
+    )
+    # Evaluation :
+    y_prob = model.predict(X_test).flatten() 
+    y_pred = (y_prob >= 0.3).astype(int) 
+    return y_pred, y_prob
